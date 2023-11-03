081b25711a4afadbafbabf4cd17d90cf8b81fc0f
sambhit12 <143902742+sambhit12@users.noreply.github.com>
2023-11-03 14:14:58 +0530

    Create BubbleSort.java

---
+++ AD-1/Class4 (13.10)/BubbleSort.java
@@ -0,0 +1,31 @@
+
+public class BubbleSort {
+	
+	public static void print(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+	
+	public static void bubble(int[] arr ) {
+		int n = arr.length;
+		for(int i = 0; i < n-1; i++) {
+			for(int j = 0; j < n-i-1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {48, 21, 34, 78, 54};
+		print(arr);
+		bubble(arr);
+		print(arr);
+	}
+
+}
