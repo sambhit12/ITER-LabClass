749ffef889e684faa2968c5419d68d990a21ae2e
sambhit12 <143902742+sambhit12@users.noreply.github.com>
2023-11-10 14:21:38 +0530

    Add files via upload

---
+++ AD-1/Class7 (10.11)/FindMissingNumber.java
@@ -0,0 +1,21 @@
+public class FindMissingNumber {
+	
+	public static int missing(int[] arr) {
+		int n = arr.length;
+		int num = n+1;
+		int totalSum = num*(num+1)/2;
+		int sum = 0;
+		int missing = 0;
+		for(int i = 0; i < n; i++) {
+			sum += arr[i];
+			missing = totalSum - sum;
+		}
+		return missing;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1, 5, 4, 3, 2, 7, 8 , 9};
+		System.out.println("Missing Number present in an array is : "+missing(arr));
+	}
+}
